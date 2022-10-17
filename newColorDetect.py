3c1b917ab70b4d3b8d5450ddeaf04e4be81a2fec
kosi-eguchi <83669620+kosi-eguchi@users.noreply.github.com>
2022-10-17 15:02:33 -0700

    Update newColorDetect.py

--- newColorDetect.py
+++ newColorDetect.py
@@ -14,8 +14,7 @@ def show_img_compar(img_1, img_2):
     ax[0].axis('off') #hide the axis
     ax[1].axis('off')
     f.tight_layout()
-    plt.show()
-
+    plt.show(block = False)
 
 def palette(clusters):
     width=300
@@ -25,19 +24,17 @@ def palette(clusters):
         palette[:, int(idx*steps):(int((idx+1)*steps)), :] = centers
     return palette
 
-
 def color_palette(image):
-    clt = KMeans(n_clusters=5)
+    clt = KMeans(n_clusters=50)
     clt_1 = clt.fit((image.reshape(-1, 3)))
     color_data = clt.cluster_centers_
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     palImg = cv2.cvtColor(palette(clt_1), cv2.COLOR_BGR2RGB)
     show_img_compar(image, palImg)
     return(color_data)
-
-
+ 
 def read_file():
-    file = open('color_data.txt','r')
+    file = open('colorData.txt','r')
     named_list = []
     for line in file:
         line = line.strip()
@@ -57,67 +54,27 @@ def color_compare(data):
         pic_r = int(col[2])
         for color in named_list:
             diff = 0
-            diff = abs(pic_r - int(color[2]))
-            diff += abs(pic_g - int(color[3]))
-            diff += abs(pic_b - int(color[4]))
+            diff = abs(pic_r - int(color[3]))
+            diff += abs(pic_g - int(color[4]))
+            diff += abs(pic_b - int(color[5]))
             diff_list.append(diff)
         diff = min(diff_list)
         # print(diff)
         index = diff_list.index(diff)
         # print(index)
-        name = named_list[index][0]
-        hex = named_list[index][1]
-        r_named = int(named_list[index][2])
-        g_named = int(named_list[index][3])
-        b_named = int(named_list[index][4])
-        compare_list.append([name, hex, r_named, g_named, b_named])
-
-
-    """
-    for col in data:
-        # print(col)
-        diff_r = 256
-        diff_g = 256
-        diff_b = 256
-        name = ""
-        hex = ""
-        r_named = 256
-        g_named = 256
-        b_named = 256
-        pic_b = int(col[0])
-        pic_g = int(col[1])
-        pic_r = int(col[2])
-        for color in named_list:
-            new_name = color[0]
-            new_hex = color[1]
-            new_r_named = int(color[2])
-            new_g_named = int(color[3])
-            new_b_named = int(color[4])
-            new_diff_r = abs(pic_r - new_r_named)
-            new_diff_g = abs(pic_g - new_g_named)
-            new_diff_b = abs(pic_b - new_b_named)
-            # print(new_r_named, new_g_named, new_b_named, new_diff_r, new_diff_g, new_diff_b)
-            if new_diff_r <= diff_r and new_diff_g <= diff_g and new_diff_b <= diff_b:
-                diff_r = new_diff_r
-                diff_g = new_diff_g
-                diff_b = new_diff_b
-                r_named = new_r_named
-                g_named = new_g_named
-                b_named = new_b_named
-                name = new_name
-                hex = new_hex
-        print("diff: %i, %i, %i" % (diff_r, diff_g, diff_b))
-        print("named: %s, %s, %i, %i, %i" % (name, hex, r_named, g_named, b_named))
-        compare_list.append([name, hex, r_named, g_named, b_named])
-    """
+        category = named_list[index][0]
+        name = named_list[index][1]
+        hex = named_list[index][2]
+        r_named = int(named_list[index][3])
+        g_named = int(named_list[index][4])
+        b_named = int(named_list[index][5])
+        compare_list.append([category, name, hex, r_named, g_named, b_named])   `       `
     val = 1
     for cols in compare_list:
-        print("%i. Name: %s, Hex Value: %s, R: %i, G: %i, B: %i" % (val, cols[0], cols[1], cols[2], cols[3], cols[4]))
+        print("%i. Name: %s, Hex Value: %s, R: %i, G: %i, B: %i, Category: %s" % (val, cols[1], cols[2], cols[3], cols[4], cols[5], cols[0]))
         val += 1
-    return compare_list
+    return compare_list             # [category, name, hex, r-val, g-val, b-val]
     
-            
-
 def color_extrapolate(image, data):
     #-------------------------------------------------------------------------------------------------------------------------------#
     # assigning and masking color 1
@@ -125,70 +82,132 @@ def color_extrapolate(image, data):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)                            # converting image to the HSV color space (chpt. 6)
 
     #-------------------------------------------------------------------------------------------------------------------------------#
-    low1 = np.array([data[0][2] -  50, data[0][1] -  50, data[0][0] -  50])             
-    high1 = np.array([data[0][2] +  50, data[0][1] +  50, data[0][0] +  50])             
-    mask1 = cv2.inRange(hsv, low1, high1)
-    extrapolate_color1 = cv2.bitwise_and(image, image, mask = mask1)
-    cv2.imshow("Mask Color 1", mask1)
-    cv2.imshow("One", extrapolate_color1)
+    # assigning and masking Green 
+    low_green = np.array([36, 50, 70])             
+    high_green = np.array([89, 255, 255])            
+    green_mask = cv2.inRange(hsv, low_green, high_green)
+    green = cv2.bitwise_and(image, image, mask = green_mask)
+    #cv2.imshow("Green Mask", green_mask)
+    #cv2.imshow("Green", green)
 
     #-------------------------------------------------------------------------------------------------------------------------------#
-    # assigning and masking color 2
-    low2 = np.array([data[1][2] -  50, data[1][1] -  50, data[1][0] -  50])             
-    high2 = np.array([data[1][2] +  50, data[1][1] +  50, data[1][0] +  50])            
-    mask2 = cv2.inRange(hsv, low2, high2)
-    extrapolate_color2 = cv2.bitwise_and(image, image, mask = mask2)
-    cv2.imshow("Mask Color 2", mask2)
-    cv2.imshow("Two", extrapolate_color2)
+    # assigning and masking Red 1
+    low_red1 = np.array([159, 50, 70])             
+    high_red1 = np.array([180, 255, 255])            
+    red1_mask = cv2.inRange(hsv, low_red1, high_red1)
+    red1 = cv2.bitwise_and(image, image, mask = red1_mask)
+    #cv2.imshow("Red 1 Mask", red1_mask)
+    #cv2.imshow("Red 1", red1)
 
     #-------------------------------------------------------------------------------------------------------------------------------#
-    # assigning and masking color 3
-    low3 = np.array([data[2][2] -  50, data[2][1] -  50, data[2][0] -  50])             
-    high3 = np.array([data[2][2] +  50, data[2][1] +  50, data[2][0] +  50])            
-    mask3 = cv2.inRange(hsv, low3, high3)
-    extrapolate_color3 = cv2.bitwise_and(image, image, mask = mask3)
-    cv2.imshow("Mask Color 3", mask3)
-    cv2.imshow("Three", extrapolate_color3)
+    # assigning and masking Red 2
+    low_red2 = np.array([0, 50, 70])             
+    high_red2 = np.array([9, 255, 255])            
+    red2_mask = cv2.inRange(hsv, low_red2, high_red2)
+    red2 = cv2.bitwise_and(image, image, mask = red2_mask)
+    #cv2.imshow("Red 2 Mask", red2_mask)
+    #cv2.imshow("Red 2", red2)
 
     #-------------------------------------------------------------------------------------------------------------------------------#
-    # assigning and masking color 4
-    low4 = np.array([data[3][2] -  50, data[3][1] -  50, data[3][0] -  50])              
-    high4 = np.array([data[3][2] +  50, data[3][1] +  50, data[3][0] +  50])            
-    mask4 = cv2.inRange(hsv, low4, high4)
-    extrapolate_color4 = cv2.bitwise_and(image, image, mask = mask4)
-    cv2.imshow("Mask Color 4", mask4)
-    cv2.imshow("Four", extrapolate_color4)
+    # assigning and masking Blue
+    low_blue = np.array([90, 50, 70])             
+    high_blue = np.array([128, 255, 255])            
+    blue_mask = cv2.inRange(hsv, low_blue, high_blue)
+    blue = cv2.bitwise_and(image, image, mask = blue_mask)
+    #cv2.imshow("Blue Mask", blue_mask)
+    #cv2.imshow("Blue", blue)
 
     #-------------------------------------------------------------------------------------------------------------------------------#
-    # assigning and masking color 5
-    low5 = np.array([data[4][2] -  50, data[4][1] -  50, data[4][0] -  50])              
-    high5 = np.array([data[4][2] +  50, data[4][1] +  50, data[4][0] + 50])            
-    mask5 = cv2.inRange(hsv, low5, high5)
-    extrapolate_color5 = cv2.bitwise_and(image, image, mask = mask5)
-    cv2.imshow("Mask Color 5", mask5)
-    cv2.imshow("Five", extrapolate_color5)
+    # assigning and masking White
+    low_white = np.array([0, 0, 231])             
+    high_white = np.array([180, 18, 255])            
+    white_mask = cv2.inRange(hsv, low_white, high_white)
+    white = cv2.bitwise_and(image, image, mask = white_mask)
+    #cv2.imshow("White Mask", white_mask)
+    #cv2.imshow("White", white)
 
     #-------------------------------------------------------------------------------------------------------------------------------#
-    """
+    # assigning and masking Black
+    low_black = np.array([180, 255, 30])             
+    high_black = np.array([0, 0, 0])            
+    black_mask = cv2.inRange(hsv, low_black, high_black)
+    black = cv2.bitwise_and(image, image, mask = black_mask)
+    #cv2.imshow("Black Mask", black_mask)
+    #cv2.imshow("Black", black)
+
+    #-------------------------------------------------------------------------------------------------------------------------------#
+    # assigning and masking Yellow
+    low_yellow = np.array([25, 50, 70])             
+    high_yellow = np.array([35, 255, 255])            
+    yellow_mask = cv2.inRange(hsv, low_yellow, high_yellow)
+    yellow = cv2.bitwise_and(image, image, mask = yellow_mask)
+    #cv2.imshow("Yellow Mask", yellow_mask)
+    #cv2.imshow("Yellow", yellow)
+
+    #-------------------------------------------------------------------------------------------------------------------------------#
+    # assigning and masking Purple
+    low_purple = np.array([129, 50, 70])             
+    high_purple = np.array([158, 255, 255])            
+    purple_mask = cv2.inRange(hsv, low_purple, high_purple)
+    purple = cv2.bitwise_and(image, image, mask = purple_mask)
+    #cv2.imshow("Purple Mask", purple_mask)
+    #cv2.imshow("Purple", purple)
+
+    #-------------------------------------------------------------------------------------------------------------------------------#
+    # assigning and masking Orange
+    low_orange = np.array([10, 50, 70])             
+    high_orange = np.array([24, 255, 255])            
+    orange_mask = cv2.inRange(hsv, low_orange, high_orange)
+    orange = cv2.bitwise_and(image, image, mask = orange_mask)
+    #cv2.imshow("Orange Mask", orange_mask)
+    #cv2.imshow("Orange", orange)
+
+    #-------------------------------------------------------------------------------------------------------------------------------#
+    # assigning and masking Gray
+    low_gray = np.array([0, 0, 40])             
+    high_gray = np.array([180, 18, 230])            
+    gray_mask = cv2.inRange(hsv, low_gray, high_gray)
+    gray = cv2.bitwise_and(image, image, mask = gray_mask)
+    #cv2.imshow("Gray Mask", gray_mask)
+    #cv2.imshow("Gray", gray)
+
+    #-------------------------------------------------------------------------------------------------------------------------------#
+    # user input (THIS PART DOESN'T WORK AND IDK WHY...ONLY SHOWS GREEN)
+    #"""
     color_search = input(str("What color are you looking for: "))
     print(color_search)
 
-    if color_search == "one" or color_search == "One":
-        cv2.imshow("Mask Color 1", mask1)
-        cv2.imshow("One", extrapolate_color1)
-    elif color_search == "two" or  color_search == "Two":
-        cv2.imshow("Mask Color 2", mask2)
-        cv2.imshow("Two", extrapolate_color2)
-    elif color_search == "three" or  color_search == "Three":
-        cv2.imshow("Mask Color 3", mask3)
-        cv2.imshow("Three", extrapolate_color3)
-    elif color_search == "four" or  color_search == "Four":
-        cv2.imshow("Mask Color 4", mask4)
-        cv2.imshow("%s" % Four, extrapolate_color4)
-    elif color_search == "five" or color_search == "Five":
-        cv2.imshow("Mask Color 5", mask5)
-        cv2.imshow("%s" % Five, extrapolate_color5)
-    """
+    if color_search == "green" or color_search == "Green":
+        cv2.imshow("Green Mask", green_mask)
+        cv2.imshow("Green", green)
+    elif color_search == "red" or  color_search == "Red":
+        cv2.imshow("Red 1 Mask", red1_mask)
+        cv2.imshow("Red 1", red1)
+    elif color_search == "red" or  color_search == "Red":
+        cv2.imshow("Red 2 Mask", red2_mask)
+        cv2.imshow("Red 2", red2)
+    elif color_search == "blue" or  color_search == "Blue":
+        cv2.imshow("Blue Mask", blue_mask)
+        cv2.imshow("Blue", blue)
+    elif color_search == "black" or  color_search == "Black":
+        cv2.imshow("Black Mask", black_mask)
+        cv2.imshow("Black", black)
+    elif color_search == "white" or color_search == "White":
+        cv2.imshow("White Mask", white_mask)
+        cv2.imshow("White", white)
+    elif color_search == "yellow" or color_search == "Yellow":
+        cv2.imshow("Yellow Mask", yellow_mask)
+        cv2.imshow("Yellow", yellow)
+    elif color_search == "purple" or color_search == "Purple":  
+        cv2.imshow("Purple Mask", purple_mask)
+        cv2.imshow("Purple", purple)
+    elif color_search == "orange" or  color_search == "Orange":
+        cv2.imshow("Orange Mask", orange_mask)
+        cv2.imshow("Orange", orange)
+    elif color_search == "gray" or  color_search == "Gray":
+        cv2.imshow("Gray Mask", gray_mask)
+        cv2.imshow("Gray", gray)
+    #"""
     cv2.waitKey(0)
     
 def main():
@@ -206,6 +225,6 @@ def main():
     print(data)
     frequent = color_compare(data)
     
-    # color_extrapolate(image, data)
+    color_extrapolate(image, data)
 
 main()
